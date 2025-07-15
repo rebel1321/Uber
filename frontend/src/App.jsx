2ae1b1da3ab941de579033079a6f781b43936a55
Satyam <www.satyam200413@gmail.com>
2025-07-15 18:19:03 +0530

    Final Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 import CaptainLogout from './pages/CaptainLogout'
 import Riding from './pages/Riding'
-import CaptainRiding from './pages/Captainriding'
+import CaptainRiding from './pages/CaptainRiding'
 
 const App = () => {
   return (
